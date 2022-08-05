65607f7f2feb5deccb3e79f3c3c2bd1d8e1cfa1b
Apex4598 <login2ajit@gmail.com>
2022-08-05 08:00:09 +0530

    Welcome to Emplyee Wage Calculation Program

---
+++ src/com/Bridgelabz/EmpWage.java
@@ -0,0 +1,9 @@
+package com.Bridgelabz;
+
+public class EmpWage {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee\n" +
+                "Wage Computation\n" +
+                "Program");
+    }
+}
